2a5b2724c6ddcc7a4a8074a4df107ba645574f8a
chamithshirantha <chamithshirantha.github@gmail.com>
2023-04-15 23:00:37 +0530

    new1 commit

--- src/app/Http/Controllers/AppController.php
+++ src/app/Http/Controllers/AppController.php
@@ -8,13 +8,13 @@ use Illuminate\Http\Request;
 class AppController extends Controller
 {
     public function index(){
-        // $data = new AppModel();
-        // $data->name = "chamith";
-        // $data->age = 13;
-        // $data->save();
+        $data = new AppModel();
+        $data->name = "chamith";
+        $data->age = 13;
+        $data->save();
 
-        $all = AppModel::all();
+        // $all = AppModel::all();
 
-        return view('home',compact('all'));
+        return view('home');
     }
 }
